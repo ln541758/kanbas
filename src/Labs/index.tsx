44051740c53b59796762ef1ff97083af9b9e449b
003 <apple@003-MacBook-Air.local>
2024-05-07 08:38:16 -0700

    first commit

---
+++ src/Labs/index.tsx
@@ -0,0 +1,14 @@
+import Lab1 from "./Lab1";
+import Lab2 from "./Lab2";
+import Lab3 from "./Lab3";
+
+export default function Labs() {
+    return (
+        <div>
+            <h1>Labs</h1>
+            <Lab1 />
+            <Lab2 />
+            <Lab3 />
+        </div>
+    );
+}
