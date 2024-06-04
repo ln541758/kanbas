b0dd53aaedb1a9541abcef98607dff518154a331
003 <apple@003-MacBook-Air.local>
2024-06-04 12:31:30 -0700

    a2 CSS and Bootstrap

--- src/Labs/Lab2/BootstrapNavigation.tsx
+++ src/Labs/Lab2/BootstrapNavigation.tsx
@@ -24,7 +24,8 @@ export default function BootstrapNavigation() {
                 <div className="card"
                     style={{ width: "18rem" }}>
                     <img src="images/stacked.jpg"
-                        className="card-img-top" />
+                        className="card-img-top"
+                        alt="" />
                     <div className="card-body">
                         <h5 className="card-title">
                             Stacking Starship
--- src/Labs/Lab2/Float.tsx
+++ src/Labs/Lab2/Float.tsx
@@ -1,23 +1,28 @@
 import "./index.css";
 
 export default function Float() {
-    return (
-        <div id="wd-css-floats">
-            <div id="wd-float-divs">
-                <h2>Float</h2>
-                <div>
-                    <div className="wd-float-left wd-dimension-portrait wd-bg-color-yellow">
-                        Yellow </div>
-                    <div className="wd-float-left wd-dimension-portrait wd-bg-color-blue wd-fg-color-white">
-                        Blue </div>
-                    <div className="wd-float-left wd-dimension-portrait wd-bg-color-red">
-                        Red </div>
-                    <img className="wd-float-right"
-                        src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg" />
-                    <div className="wd-float-done"></div>
-                </div>
-            </div>
-
+  return (
+    <div id="wd-css-floats">
+      <div id="wd-float-divs">
+        <h2>Float</h2>
+        <div>
+          <div className="wd-float-left wd-dimension-portrait wd-bg-color-yellow">
+            Yellow{" "}
+          </div>
+          <div className="wd-float-left wd-dimension-portrait wd-bg-color-blue wd-fg-color-white">
+            Blue{" "}
+          </div>
+          <div className="wd-float-left wd-dimension-portrait wd-bg-color-red">
+            Red{" "}
+          </div>
+          <img
+            className="wd-float-right"
+            alt=""
+            src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"
+          />
+          <div className="wd-float-done"></div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
